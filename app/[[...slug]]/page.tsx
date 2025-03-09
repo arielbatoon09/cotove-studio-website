3242d8f10dcec9adde2a511e2b25669a8e842fcd
Ariel Batoon <info.arielbatoon@gmail.com>
2025-03-09 12:22:42 +0800

    test22

--- app/[[...slug]]/page.tsx
+++ app/[[...slug]]/page.tsx
@@ -4,9 +4,10 @@ import { getPageData } from "@/sanity/query/getPageData";
 import { TPageProps } from "@/types";
 import { notFound } from "next/navigation";
 
-export async function generateMetadata({ params }: { params: { slug: string[] } }): Promise<Metadata> {
+export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
   const defaultPage = process.env.DEFAULT_HOME_SLUG as string;
-  const resolved = params.slug?.[0] ?? defaultPage;
+  const { slug } = await params;
+  const resolved = !slug ? defaultPage : slug[0];
   return await createMetadata(resolved);
 }
 
