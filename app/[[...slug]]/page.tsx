a0b52c6376fae877d8b860285f965ece3a200146
Ariel Batoon <info.arielbatoon@gmail.com>
2025-03-09 12:56:17 +0800

    testing new params approach

--- app/[[...slug]]/page.tsx
+++ app/[[...slug]]/page.tsx
@@ -1,19 +1,21 @@
-import type { Metadata } from "next";
-import { createMetadata } from "@/lib/metadata";
+// import type { Metadata } from 'next';
+// import { createMetadata } from "@/lib/metadata";
 import { getPageData } from "@/sanity/query/getPageData";
 import { TPageProps } from "@/types";
 import { notFound } from "next/navigation";
 
-export async function generateMetadata({ params }: { params: { slug?: string[] } }): Promise<Metadata> {
-  const defaultPage = process.env.DEFAULT_HOME_SLUG as string;
-  const resolved = params.slug?.[0] ?? defaultPage;
-  return createMetadata(resolved);
-}
+// export async function generateMetadata({ params }: { params: { slug?: string[] } }): Promise<Metadata> {
+//   const defaultPage = process.env.DEFAULT_HOME_SLUG as string;
+//   const { slug } = await params;
+//   const resolved = !slug ? defaultPage : slug[0];
+//   return await createMetadata(resolved);
+// }
 
 // Dynamic Page Handler
-export default async function Page({ params }: { params: { slug?: string[] } }) {
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
   const defaultPage = process.env.DEFAULT_HOME_SLUG as string;
-  const resolved = params.slug?.[0] ?? defaultPage;
+  const { slug } = await params;
+  const resolved = !slug ? defaultPage : slug[0];
   const page = await getPageData(resolved);
 
   if (!page) {
@@ -25,7 +27,9 @@ export default async function Page({ params }: { params: { slug?: string[] } }) 
 
 // Render Pages
 function renderPage(page: TPageProps) {
-  return <div>{page.title}</div>;
+  return (
+    <div>{page.title}</div>
+  );
 }
 
 export const revalidate = 1;
